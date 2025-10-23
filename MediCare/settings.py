c1e97b14fa91e57686a5798ab7337ba6cfa080df
hnkrupaal <hnkrupaal@gmail.com>
2025-10-24 00:30:43 +0530

    Update DEBUG and ALLOWED_HOSTS in settings.py
    Changed DEBUG setting to False and updated ALLOWED_HOSTS.

--- MediCare/settings.py
+++ MediCare/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-iww6k6cq=!g1$p7yfr!)1#-!)#dg^^0ew=+6hwu^z4u)#3tk)%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['krupaal.pythonanywhere.com']
 
 
 # Application definition
@@ -119,8 +119,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
